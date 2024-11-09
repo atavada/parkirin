0ed39dc060e4220666b3683d7598451fdefba652
Adil Zakaria <adilzakaria18@gmail.com>
2024-11-09 19:44:27 +0700

    auth-mitra

---
+++ src/app/(auth)/store/sign-in/page.tsx
@@ -0,0 +1,21 @@
+import { SignInFormStore } from "@/components/store-auth/SignInFormStore";
+import Image from "next/image";
+
+export default function Page() {
+	return (
+		<div className="flex justify-center items-center min-h-screen">
+			<div className="space-y-6 p-4">
+				<div className="rounded-xl shadow">
+					<Image
+						alt="banner"
+						className="h-40 w-full rounded-xl mx-auto"
+						height="550"
+						src="/dummy-image.jpg"
+						width="550"
+					/>
+				</div>
+				<SignInFormStore />
+			</div>
+		</div>
+	);
+}
